3ce8f94e0d060e9607b7ba819e7743cec0a202d3
unknown <durgeshsingh12712@gmail.com>
2025-08-10 16:22:57 +0530

    Tools

---
+++ youtube/utils/__init__.py
@@ -0,0 +1,19 @@
+from .tools import (
+    read_yaml,
+    create_directories,
+    save_json,
+    load_json,
+    save_bin,
+    load_bin,
+    get_size
+)
+
+__all__ = [
+    "read_yaml",
+    "create_directories",
+    "save_json",
+    "load_json",
+    "save_bin",
+    "load_bin",
+    "get_size"
+]
---
+++ youtube/utils/tools.py
@@ -0,0 +1,115 @@
+import os, yaml, json, pickle
+from pathlib import Path
+from box import ConfigBox
+from box.exceptions import BoxValueError
+from ensure import ensure_annotations
+
+from youtube.loggers import logger
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+    
+    Args:
+        path_to_yaml (str): path like input
+        
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+        
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("YAML file is empty")
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+    
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"Created Directory at : {path}")
+
+
+@ensure_annotations
+def  save_json(path: Path, data: dict):
+    """save json data
+    
+    Args:
+        path (Path): path to json file
+        data (dict): data to be saved in json file
+    """
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+    
+    logger.info(f"JSON file saved at: {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+    
+    Args:
+        path (Path): path to json file
+        
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
+    with open(path) as f:
+        content = json.load(f)
+    logger.info(f"JSON file loaded successfully from: {path}")
+    return ConfigBox(content)
+
+def save_bin(data, path: str):
+    """
+    Save binary data using pickle
+    """
+    try:
+        dir_path = os.path.dirname(path)
+        os.makedirs(dir_path, exist_ok= True)
+
+        with open(path, 'wb') as f:
+            pickle.dump(data, f)
+    
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def load_bin(path: Path):
+    """load binary data
+    
+    Args:
+        path (Path): path to binary file
+        
+    Returns:
+        Any: object stored in the file
+    """
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+    logger.info(f"Binary File loaded from: {path}")
+    return data
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+    
+    Args:
+        path (Path): path of the file
+        
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~{size_in_kb}KB"
